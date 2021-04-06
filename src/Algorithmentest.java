3f713fb131b1203f49f747407845828a04a709c6
Tobias Weidenfeller <tobias.weidenfeller99@gmail.com>
2021-04-06 20:45:42 +0200

    feat: added insertionsort algorithm

--- src/Algorithmentest.java
+++ src/Algorithmentest.java
@@ -1,7 +1,27 @@
+import java.util.Arrays;
+import java.util.Formatter;
 
 public class Algorithmentest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		int[] unsorted = { 8, 7, 2, 9, 234, 412, 21, 4, 975, 42, 278 };
+		System.out.println("Unsorted: " + Arrays.toString(unsorted));
+
+		Insertionsort insertionsort = new Insertionsort();
+
+		long startTime = System.nanoTime();
+
+		int[] sorted = insertionsort.sort(unsorted, false);
+
+		long endTime = System.nanoTime();
+		long calcTime = endTime - startTime;
+
+		System.out.println("Sorted: " + Arrays.toString(sorted));
+		System.out.println("Berechnungszeit: " + calcTime + " nanoseconds");
+
+		Formatter formatter = new Formatter();
+		Formatter a = formatter.format("%s %<s %s", "a", "b", "c", "d");
+		System.out.println(a);
+		formatter.close();
 	}
 }
---
+++ src/Insertionsort.java
@@ -0,0 +1,34 @@
+public class Insertionsort {
+    public int[] sort(int[] array, boolean detailed) {
+        for (int i = 0; i < array.length; i++) {
+            int entry = array[i];
+            int j = i;
+            while (j > 0 && array[j - 1] > entry) {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = entry;
+
+            if (detailed) {
+                System.out.println(i + " --> " + j);
+                StringBuilder sb = new StringBuilder();
+                for (int k = 0; k < array.length; k++) {
+                    boolean close = false;
+                    if (k == j) {
+                        sb.append("\u001B[31m");
+                        close = true;
+                    }
+                    sb.append(array[k]);
+                    if (close) {
+                        sb.append("\u001B[0m");
+                    }
+                    sb.append(", ");
+                }
+                System.out.println(sb);
+                System.out.println();
+            }
+
+        }
+        return array;
+    }
+}
